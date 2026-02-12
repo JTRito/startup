15f57efc7db311ffbbc70edf23d51bbcc8beb690
juanotherito <juanotheunalpo@gmail.com>
2026-02-12 16:43:10 -0700

    change class references to className

--- src/app.jsx
+++ src/app.jsx
@@ -4,18 +4,18 @@ import './app.css';
 
 export default function App() {
     return <div className="body bg-light text-dark">
-        <header class="container-fluid bg-body">
-            <nav class="navbar fixed-top navbar-light bg-light">
-                <a class="navbar-brand" href="#">BGTimer</a>
-                <menu class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
+        <header className="container-fluid bg-body">
+            <nav className="navbar fixed-top navbar-light bg-light">
+                <a className="navbar-brand" href="#">BGTimer</a>
+                <menu className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="index.html">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="join.html">Join</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="join.html">Join</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="game.html">Game</a>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="game.html">Game</a>
                     </li>
                 </menu>
             </nav>
@@ -24,11 +24,11 @@ export default function App() {
         <main>App Components Go here</main>
 
         <footer>
-            <div class="container-fluid">
-                <span class="text-reset">Jacob Unalp</span>
-                <a class="text-reset" href="https://github.com/JuanotheRito/startup">GitHub</a>
+            <div className="container-fluid">
+                <span className="text-reset">Jacob Unalp</span>
+                <a className="text-reset" href="https://github.com/JuanotheRito/startup">GitHub</a>
             </div>
         </footer>
-        
+
     </div>;
 }
